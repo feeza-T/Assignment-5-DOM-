50ae5c4a92c42a4fc82973271f5ae982fb9609e2
feeza-T <takwafeeza84@gmail.com>
2024-09-24 03:29:55 +0600

    Done the colour changing part of the buttons

--- features.js
+++ features.js
@@ -3,6 +3,15 @@ function showSectionById(id) {
     document.getElementById('donation-page').classList.add('hidden');
     document.getElementById('history-page').classList.add('hidden');
 
+    //button colour change
+    if (id === 'donation-page') {
+        document.getElementById('btn-donation').classList.add('bg-btn-bg');
+        document.getElementById('btn-history').classList.remove('bg-btn-bg');
+      } else if (id === 'history-page') {
+        document.getElementById('btn-history').classList.add('bg-btn-bg');
+        document.getElementById('btn-donation').classList.remove('bg-btn-bg');
+      }
+
     document.getElementById(id).classList.remove('hidden');
   }
   
